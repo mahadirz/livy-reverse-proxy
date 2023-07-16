a63f77bda146e04f06b4d2200ecdbae32d73c41d
mahadirz <mahadirz.93@gmail.com>
2023-07-16 12:18:42 +0800

    Add the codes

---
+++ proxy.py
@@ -0,0 +1,198 @@
+import argparse
+import base64
+import configparser
+import gzip
+import hashlib
+import json
+import logging
+import re
+import select
+import socket
+import sys
+import time
+import traceback
+
+
+class Config:
+    def __init__(self, server_address, forward_to, shiro_path, log_level):
+        self.buffer_size = 4096
+        self.delay = 0.0001
+        self.server_address = server_address
+        self.forward_to = forward_to
+        self.shiro_path = shiro_path
+        self.log_level = log_level
+
+
+class Shiro:
+    def __init__(self, path):
+        config = configparser.ConfigParser()
+        config.read(path)
+        self.users = self._extract_users(config)
+
+    @staticmethod
+    def _extract_users(config):
+        return {
+            user: {
+                "iteration": int(auth_user[3]),
+                "salt": auth_user[4],
+                "hashed": auth_user[5]
+            }
+            for user, auth_user in ((user, config['users'][user].split(",")[0].split("$")) for user in config['users'])
+        }
+
+    def authenticate(self, user, password):
+        """
+        Original Code in Java https://github.com/apache/shiro/blob/b7253552e4fa12df4f40557087addba8888e147d/crypto/hash/src/main/java/org/apache/shiro/crypto/hash/SimpleHash.java#L387
+        :param user:
+        :param password:
+        :return:
+        """
+        if user not in self.users:
+            return False
+
+        hashed = self.users[user]
+
+        m = hashlib.sha256()
+        m.update(base64.b64decode(hashed['salt']))
+        m.update(password.encode())
+        digest = m.digest()
+
+        for _ in range(hashed['iteration'] - 1):
+            digest = hashlib.sha256(digest).digest()
+
+        return base64.b64encode(digest).decode() == hashed['hashed']
+
+
+class ProxyServer:
+    def __init__(self, config):
+        self.config = config
+        self.shiro = Shiro(config.shiro_path)
+        self.input_list = []
+        self.channel = {}
+        self.server = self._init_server(*config.server_address)
+
+    def _init_server(self, host, port):
+        server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        server.bind((host, port))
+        server.listen(200)
+        return server
+
+    def main_loop(self):
+        self.input_list.append(self.server)
+        while True:
+            time.sleep(self.config.delay)
+            inputready, _, _ = select.select(self.input_list, [], [])
+            for self.s in inputready:
+                if self.s == self.server:
+                    self.on_accept()
+                    break
+
+                self.data = self.s.recv(self.config.buffer_size)
+                if len(self.data) == 0:
+                    self.on_close()
+                    break
+                else:
+                    self.on_recv()
+
+    def on_accept(self):
+        forward = self._init_forward(*self.config.forward_to)
+        clientsock, clientaddr = self.server.accept()
+        if forward:
+            logging.info(f'{clientaddr[0]}:{clientaddr[1]} has connected')
+            self.input_list.append(clientsock)
+            self.input_list.append(forward)
+            self.channel[clientsock] = {"socket": forward, "type": "client"}
+            self.channel[forward] = {"socket": clientsock, "type": "forward"}
+        else:
+            logging.warning(
+                f"Can't establish connection with remote server. Closing connection with client side {clientaddr}")
+            clientsock.close()
+
+    @staticmethod
+    def _init_forward(host, port):
+        try:
+            forward = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            forward.connect((host, port))
+            return forward
+        except Exception:
+            logging.error(traceback.format_exc())
+            return False
+
+    def on_close(self):
+        logging.info(f"{self.s.getpeername()} has disconnected")
+        self.input_list.remove(self.s)
+        self.input_list.remove(self.channel[self.s]["socket"])
+        out = self.channel[self.s]["socket"]
+        self.channel[out]["socket"].close()
+        self.channel[self.s]["socket"].close()
+        del self.channel[out]
+        del self.channel[self.s]
+
+    def on_recv(self):
+        if self.channel[self.s]['type'] == "client":
+            self.process_client_data()
+        else:
+            self.channel[self.s]["socket"].sendall(self.data)
+
+    def process_client_data(self):
+        """
+        
+        :return: 
+        """
+        try:
+            headers, body = self.data.split(b'\r\n\r\n', 1)
+            header_lines = headers.decode().split('\r\n')
+            method, path, _ = header_lines[0].split()
+            # the following lines can be refactored as plugin based
+            if method == 'POST' and path.startswith('/sessions') and not re.match(r"\/sessions\/\d+?\/statements",
+                                                                                  path):
+                if not self.authenticate_request(header_lines, body):
+                    return self.send_auth_error()
+            self.channel[self.s]["socket"].sendall(self.data)
+        except Exception:
+            logging.error(traceback.format_exc())
+
+    def authenticate_request(self, header_lines, body):
+        content_encoding, content_type, headers = self.parse_headers(header_lines)
+        if content_type == 'application/json':
+            body = gzip.decompress(body) if content_encoding == 'gzip' else body
+            post_data = json.loads(body)
+            if 'proxyUser' in post_data and 'proxyPassword' in headers:
+                return self.shiro.authenticate(post_data['proxyUser'], headers['proxyPassword'])
+        return False
+
+    @staticmethod
+    def parse_headers(header_lines):
+        headers = dict(line.split(":", 1) for line in header_lines[1:] if ":" in line)
+        return headers.get('content-encoding'), headers.get('content-type'), headers
+
+    @staticmethod
+    def send_auth_error():
+        error_message = 'Authentication Failed: Invalid proxyUser or proxyPassword'
+        response = f'HTTP/1.1 401 Unauthorized Request\r\nContent-Type: text/plain\r\nContent-Length: {len(error_message)}\r\n\r\n{error_message}'
+        self.s.sendall(response.encode())
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--server_address", nargs=2, default=('0.0.0.0', 8998), type=str, help="Server IP and port")
+    parser.add_argument("--forward_to", nargs=2, default=('127.0.0.1', 8999), type=str,
+                        help="Forwarding server IP and port")
+    parser.add_argument("--shiro_path", default="/etc/zeppelin/conf/shiro.ini", type=str, help="Path to Shiro INI")
+    parser.add_argument("--log_level", default='INFO', type=str, help="Logging level")
+    args = parser.parse_args()
+
+    logging.basicConfig(level=args.log_level, format='%(asctime)s - %(levelname)s - %(message)s')
+
+    config = Config(args.server_address, args.forward_to, args.shiro_path, args.log_level)
+
+    server = ProxyServer(config)
+
+    try:
+        logging.info(f"Listening to {config.server_address[0]}:{config.server_address[1]}")
+        logging.info(f"Forwarding host {config.forward_to[0]}:{config.forward_to[1]}")
+        server.main_loop()
+    except KeyboardInterrupt:
+        logging.info("Ctrl C - Stopping server")
+        sys.exit(1)
